98584bda32b15cc8187d2e529243f7a3d8a316a1
linklingj <jaehyunc413@gmail.com>
2022-11-28 13:02:49 +0900

    script impl

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,14 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
-{
-    CharacterAnimation c_anim;
-    void Start() {
-        c_anim = GetComponent<CharacterAnimation>();
+namespace PathCreation {
+    public class NPC : MonoBehaviour {
+        CharacterAnimation c_anim;
+        Rigidbody2D rb;
+
+        NPCController npcController;
+        public PathCreator pathCreator;
+        public int pathNum;
+        public EndOfPathInstruction endOfPathInstruction;
+        public Transform player;
+        public float targetSpeed;
+        float speed;
+        [Range(0f, 1f)]
+        public float startPoint;
+        float distanceTravelled;
+        bool active;
+        Vector2 movement;
+
+
+        void Start() {
+            c_anim = GetComponent<CharacterAnimation>();
+            rb = GetComponent<Rigidbody2D>();
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            npcController = FindObjectOfType<NPCController>();
+            targetSpeed = npcController.npcSpeed;
+            distanceTravelled = pathCreator.path.length * startPoint;
+            speed = targetSpeed;
+        }
+
+        private void Update() {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            movement = new Vector2(rb.velocity.x, rb.velocity.y).normalized;
+        }
+
+        private void FixedUpdate() {
+            c_anim.CheckAnim(movement);
+        }
+
+        public void MoveNPC() {
+            LeanTween.value(gameObject, speed, targetSpeed, 2f).setOnUpdate((x) => speed = x);
+        }
+        public void StopNPC() {
+            speed = 0;
+        }
     }
 
-    private void FixedUpdate() {
-        
-    }
 }
--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace PathCreation {
+    public class NPCController : MonoBehaviour {
+        public Transform NPCParent;
+        public GameObject NPCPrefab;
+        public PathCreator[] paths;
+        public float npcSpeed;
+        Vector3 spawnPos = new Vector3 (-1000, -1000, 0);
+        List<NPC>[] npcs = new List<NPC>[8];
+        void Start() {
+            for (int i = 0; i < 8; i++) {
+                npcs[i] = new List<NPC>();
+            }
+            /*
+            MakeCar(greenCar, 0, 0);
+            MakeCar(redMotorcycle, 0, 0.5f);
+            MakeCar(blueCar, 1, 0.25f);
+            MakeCar(bigGreenCar, 1, 0.75f);
+            //b
+            MakeCar(brownMotorcycle, 2, 0);
+            MakeCar(bigGreenCar, 2, 0.5f);
+            MakeCar(greenCar, 3, 0.25f);
+            MakeCar(whiteMotorcycle, 3, 0.75f);
+            //e
+            MakeCar(greenCar, 6, 0);
+            MakeCar(purpleCar, 6, 0.3f);
+            MakeCar(blueCar, 6, 0.6f);
+            //f
+            MakeCar(redCar, 7, 0);
+            MakeCar(redMotorcycle, 7, 0.4f);
+            MakeCar(yellowCar, 7, 0.8f);*/
+        }
+
+        void MakeCar(GameObject go, int num, float sP) {
+            GameObject npc = Instantiate(go, spawnPos, Quaternion.identity);
+            NPC pf = npc.GetComponent<NPC>();
+            pf.pathCreator = paths[num];
+            pf.startPoint = sP;
+            pf.pathNum = num;
+            npc.transform.SetParent(NPCParent);
+            npcs[num].Add(pf);
+        }
+        public void Stop(int n) {
+            foreach (NPC car in npcs[n]) {
+                car.StopNPC();
+            }
+        }
+        public void Move(int n) {
+            foreach (NPC car in npcs[n]) {
+                car.MoveNPC();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
