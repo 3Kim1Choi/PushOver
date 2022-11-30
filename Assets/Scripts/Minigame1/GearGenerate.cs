d166da2e4013c80aabfe5e75466aaeca468f5ec2
linklingj <jaehyunc413@gmail.com>
2022-11-30 10:57:40 +0900

    object pooling

--- Assets/Scripts/Minigame1/GearGenerate.cs
+++ Assets/Scripts/Minigame1/GearGenerate.cs
@@ -19,23 +19,32 @@ public class GearGenerate : MonoBehaviour
     private void Update() {
         timer += Time.deltaTime;
         if (timer > lastTime + 2f) {
+            lastTime += 2f;
             int rand = Random.Range(0,3);
             if (rand == 0) {
-                GameObject g = Instantiate(gear, new Vector3(-10, player.transform.position.y + Random.Range(4, 10), 0 ), Quaternion.identity);
+                GameObject g = ObjectPool.instance.gearQueue.Dequeue();
+                g.transform.position = new Vector3(-10, player.transform.position.y + Random.Range(4, 10), 0 );
+                g.SetActive(true);
                 g.transform.parent = gears.transform;
                 g.GetComponent<Gear>().gearType = 0;
+                g.GetComponent<Gear>().Generated();
             }
             else if (rand == 1) {
-                GameObject g = Instantiate(gear, new Vector3(10, player.transform.position.y + Random.Range(4, 10), 0 ), Quaternion.identity);
+                GameObject g = ObjectPool.instance.gearQueue.Dequeue();
+                g.SetActive(true);
+                g.transform.position = new Vector3(10, player.transform.position.y + Random.Range(4, 10), 0 );
                 g.transform.parent = gears.transform;
                 g.GetComponent<Gear>().gearType = 1;
+                g.GetComponent<Gear>().Generated();
             }
             else if (rand == 2) {
-                GameObject g = Instantiate(gear, new Vector3(player.transform.position.x + Random.Range(-1.5f,1.5f), player.transform.position.y + 16, 0), Quaternion.identity);
+                GameObject g = ObjectPool.instance.gearQueue.Dequeue();
+                g.SetActive(true);
+                g.transform.position = new Vector3(player.transform.position.x + Random.Range(-1.5f,1.5f), player.transform.position.y + 16, 0);
                 g.transform.parent = gears.transform;
                 g.GetComponent<Gear>().gearType = 2;
+                g.GetComponent<Gear>().Generated();
             }
-            lastTime += 2f;
         }
     }
 
